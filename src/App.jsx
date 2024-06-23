8ad39b9165fdb93654955ba22f0b6a7e86baadd2
sowmik-sec <sowmik.sec@gmail.com>
2024-06-23 15:38:15 +0600

    jsx rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Vite + React</h1>
-        <Person/>
+      <Person />
+      <Student />
     </>
-  )
+  );
 }
 
 function Person() {
   const age = 26;
   const money = 20;
-  const person = {name: 'sakib', age: 12};
-  return <h3>I am {person.name} with age: {age+money}</h3>
+  const person = { name: "sakib", age: 12 };
+  return (
+    <h3>
+      I am {person.name} with age: {age + money}
+    </h3>
+  );
 }
 
-export default App
+function Student() {
+  return (
+    <div>
+      <h3>This is a student</h3>
+    </div>
+  );
+}
+
+export default App;
